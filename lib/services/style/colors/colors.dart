a215e21f1d968576e99cad5e61b43dda563e0187
Freddie <124230701+Freddieth@users.noreply.github.com>
2023-03-22 01:15:15 +0200

    Add theme & colors

---
+++ lib/services/style/colors/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const Color colorgreen = Color(0xFF61E757);
+const Color colorgrey = Color(0xFFC8C9CB);
+const Color scaffoldBackgroundColorlight = Color(0xFFDFECDB);
+const Color scaffoldBackgroundColordark = Color(0xFF060E1E);
+const Color colorprimary = Color(0xFF5D9CEC);
+const Color colordark = Color(0xFF141922);

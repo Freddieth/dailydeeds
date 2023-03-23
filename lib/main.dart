5e58e4d22a89feeb4052f43a0cf59a3ddf13a933
Freddie <124230701+Freddieth@users.noreply.github.com>
2023-03-23 03:15:55 +0200

    Add shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (context) => ThemeProvider()),
     ChangeNotifierProvider(create: (context) => LanguageProvider())
-  ], child: const MyApp()));
+  ], child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  MyApp({super.key});
+  late ThemeProvider themeprovider;
+  late LanguageProvider languageprovider;
   @override
   Widget build(BuildContext context) {
-    var themeprovider = Provider.of<ThemeProvider>(context);
-    var languageprovider = Provider.of<LanguageProvider>(context);
+    themeprovider = Provider.of<ThemeProvider>(context);
+    languageprovider = Provider.of<LanguageProvider>(context);
+    initsharedpref();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: themeprovider.currenttheme,
@@ -39,5 +42,15 @@ class MyApp extends StatelessWidget {
         HomeLayout.routeName: (context) => const HomeLayout(),
       },
     );
+  }
+
+  void initsharedpref() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String lang = prefs.getString('language') ?? 'en';
+    languageprovider.changelang(lang);
+    String? theme = prefs.getString('theme');
+    theme == 'light'
+        ? themeprovider.changetheme(ThemeMode.light)
+        : themeprovider.changetheme(ThemeMode.dark);
   }
 }
--- lib/services/provider/language/languageprovider.dart
+++ lib/services/provider/language/languageprovider.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LanguageProvider extends ChangeNotifier {
   String currentlang = 'en';
-  void changelang(String newlang) {
+  void changelang(String newlang) async {
     currentlang = newlang;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('language', currentlang);
     notifyListeners();
   }
 }
